cfb20c22b0de4ab55863266ebf2cad121af44a95
sikichmatt <sikichmatt@aquinas.org>
2025-09-27 11:04:45 -0700

    added a sprite

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import kaplay from "kaplay";
+import kaplay, { Color } from "kaplay";
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
 const k = kaplay();
@@ -9,3 +9,6 @@ k.loadSprite("bean", "sprites/bean.png");
 k.add([k.pos(120, 80), k.sprite("bean")]);
 
 k.onClick(() => k.addKaboom(k.mousePos()));
+
+//add something to the screen
+add([sprite("bean"), pos(80, 40), scale(3), rotate(30), color(0, 0, 255)]);
